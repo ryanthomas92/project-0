745cdcf9bd3db948ef770877181f682b6a3a078f
Ryan_Thomas <rthoma11@villanova.edu>
2016-12-08 17:55:38 -0800

    up, down, left, and right trigger alert

--- app.js
+++ app.js
@@ -4,4 +4,31 @@ $(document).ready(function() {
     alert("start clicked");
   });
 
+  $(document).keydown(function(e){
+    if (e.keyCode == 37) {
+       alert( "left pressed" );
+       return false;
+    }
+  });
+
+  $(document).keydown(function(e){
+    if (e.keyCode == 38) {
+       alert( "up pressed" );
+       return false;
+    }
+  });
+
+  $(document).keydown(function(e){
+    if (e.keyCode == 39) {
+       alert( "right pressed" );
+       return false;
+    }
+  });
+
+  $(document).keydown(function(e){
+    if (e.keyCode == 40) {
+       alert( "down pressed" );
+       return false;
+    }
+  });
 });
